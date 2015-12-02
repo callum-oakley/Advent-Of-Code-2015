05b6ae00fcfbaaa0fb5f2a5597573e183f135304
Callum Oakley <c.oakley108@gmail.com>
2015-12-02 14:46:08 +0000

    Day 1 part 2

--- Day-1-Not-Quite-Lisp.hs
+++ Day-1-Not-Quite-Lisp.hs
@@ -1,2 +1,10 @@
+-- Part 1
+parensToInts :: String -> [Int]
+parensToInts = map (\p -> if p == '(' then 1 else -1)
+
 whatFloor :: String -> Int
-whatFloor = sum . map (\p -> if p == '(' then 1 else -1)
+whatFloor = sum . parensToInts
+
+-- Part 2
+whatPosition :: String -> Int
+whatPosition = length . takeWhile (>= 0) . scanl (+) 0 . parensToInts
