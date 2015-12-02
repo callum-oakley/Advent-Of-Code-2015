64a3d3e9711ec1a1befe414c48b8af30479fd44a
Callum Oakley <c.oakley108@gmail.com>
2015-12-02 15:28:32 +0000

    Day 2 part 2

--- Day-2-I-Was-Told-There-Would-Be-No-Math.hs
+++ Day-2-I-Was-Told-There-Would-Be-No-Math.hs
@@ -3,8 +3,15 @@ import Data.List
 
 -- we first hand process the input so that it is in the form [..[l, w, h]..]
 
-area :: [Int] -> Int
-area [x, y, z] = 3*x*y + 2*y*z + 2*z*x
+paper :: [Int] -> Int -- assumes x <= y <= z
+paper [x, y, z] = 3*x*y + 2*y*z + 2*z*x
 
-totalArea :: [[Int]] -> Int
-totalArea = sum . map area . map sort
+totalPaper :: [[Int]] -> Int
+totalPaper = sum . map paper . map sort
+
+-- Part 2
+ribbon :: [Int] -> Int -- assumes x <= y <= z
+ribbon [x, y, z] = 2*x + 2*y + x*y*z
+
+totalRibbon :: [[Int]] -> Int
+totalRibbon = sum . map ribbon . map sort
