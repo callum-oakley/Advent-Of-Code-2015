832a69bdff6d9ca65c852f11d42d7ea2df0e2630
Callum Oakley <c.oakley108@gmail.com>
2015-12-04 12:30:06 +0000

    Day 4 part 2

--- Day-4-The-Ideal-Stocking-Stuffer.hs
+++ Day-4-The-Ideal-Stocking-Stuffer.hs
@@ -1,10 +1,19 @@
+-- Part 1
 import Crypto.Hash
+import Data.List
 import qualified Data.ByteString.Lazy.Char8 as B
 
 md5 :: String -> Digest MD5
 md5 = hashlazy . B.pack
 
-naive :: String -> Int
-naive key = head . filter p $ [1..]
+naive :: String -> String -> Int
+naive prefix key = head . filter p $ [1..]
   where
-    p = (== "00000") . take 5 . show . md5 . (key ++) . show
+    p = isPrefixOf prefix . show . md5 . (key ++) . show
+
+find5zeros :: String -> Int
+find5zeros = naive "00000"
+
+-- Part 2
+find6zeros :: String -> Int
+find6zeros = naive "000000"
