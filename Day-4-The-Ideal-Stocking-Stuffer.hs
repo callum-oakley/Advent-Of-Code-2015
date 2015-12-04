84c8f6a4fccfc78db90266b7aca8c760658231cb
Callum Oakley <c.oakley108@gmail.com>
2015-12-04 12:46:54 +0000

    renamed a function

--- Day-4-The-Ideal-Stocking-Stuffer.hs
+++ Day-4-The-Ideal-Stocking-Stuffer.hs
@@ -6,14 +6,14 @@ import qualified Data.ByteString.Lazy.Char8 as B
 md5 :: String -> Digest MD5
 md5 = hashlazy . B.pack
 
-naive :: String -> String -> Int
-naive prefix key = head . filter p $ [1..]
+searchHashes :: String -> String -> Int
+searchHashes prefix key = head . filter p $ [1..]
   where
     p = isPrefixOf prefix . show . md5 . (key ++) . show
 
 find5zeros :: String -> Int
-find5zeros = naive "00000"
+find5zeros = searchHashes "00000"
 
 -- Part 2
 find6zeros :: String -> Int
-find6zeros = naive "000000"
+find6zeros = searchHashes "000000"
