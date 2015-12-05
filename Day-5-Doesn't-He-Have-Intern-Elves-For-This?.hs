d091688b927ba55a5d05d980294a80b2fe3ee110
Callum Oakley <c.oakley108@gmail.com>
2015-12-05 15:14:44 +0000

    Day 5 part 2

--- Day-5-Doesn't-He-Have-Intern-Elves-For-This?.hs
+++ Day-5-Doesn't-He-Have-Intern-Elves-For-This?.hs
@@ -6,9 +6,11 @@ hasThreeVowels = (> 2) . length . filter isVowel
   where
     isVowel c = elem c "aeiou"
 
-hasDouble :: String -> Bool
-hasDouble s = (/= []) . filter isDiagonal $ zip (tail s) s
+hasOffsetTwin :: Int -> Int -> String -> Bool
+hasOffsetTwin subLength offset s =
+    isInfixOf test . map isDiagonal $ zip (drop offset s) s
   where
+    test              = take subLength $ repeat True
     isDiagonal (x, y) = x == y
 
 hasNoBadPairs :: String -> Bool
@@ -16,10 +18,22 @@ hasNoBadPairs s = (== []) . filter (flip isInfixOf s) $ badPairs
   where
     badPairs = ["ab", "cd", "pq", "xy"]
 
-isNiceString :: String -> Bool
-isNiceString s = and . map (\check -> check s) $ checks
-  where
-    checks = [hasThreeVowels, hasDouble, hasNoBadPairs]
+isNiceString :: [String -> Bool] -> String -> Bool
+isNiceString checks s = and . map (\check -> check s) $ checks
 
-howManyNiceStrings :: [String] -> Int
-howManyNiceStrings = length . filter isNiceString
+howManyNiceStringsGeneral :: [String -> Bool] -> [String] -> Int
+howManyNiceStringsGeneral checks = length . filter (isNiceString checks)
+
+howManyNiceStringsOne :: [String] -> Int
+howManyNiceStringsOne = howManyNiceStringsGeneral
+    [hasThreeVowels, hasOffsetTwin 1 1, hasNoBadPairs]
+
+-- Part 2
+hasNonoverlappingTwinPair :: String -> Bool
+hasNonoverlappingTwinPair s = (/= []) . filter p $ [2..(length s - 2)]
+  where
+    p offset = hasOffsetTwin 2 offset s
+
+howManyNiceStringsTwo :: [String] -> Int
+howManyNiceStringsTwo = howManyNiceStringsGeneral
+    [hasNonoverlappingTwinPair, hasOffsetTwin 1 2]
