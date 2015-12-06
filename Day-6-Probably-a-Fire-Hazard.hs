cbdc249ebda0553141165efde120dcbbd69264d9
Callum Oakley <c.oakley108@gmail.com>
2015-12-06 17:12:26 +0000

    Day 6 part 2

--- Day-6-Probably-a-Fire-Hazard.hs
+++ Day-6-Probably-a-Fire-Hazard.hs
@@ -5,23 +5,21 @@
 import Data.Array
 import Data.List
 
-data Light       = Off | On deriving (Eq, Show, Read)
+type Light       = Int
 type LightGrid   = Array (Int, Int) Light
 type Instruction = (Light -> Light, (Int, Int), (Int, Int))
 
 turnOn :: Light -> Light
-turnOn _ = On
+turnOn _ = 1
 
 turnOff :: Light -> Light
-turnOff _ = Off
+turnOff _ = 0
 
 toggle :: Light -> Light
-toggle state
-    | state == On  = Off
-    | state == Off = On
+toggle = (1 -)
 
 initialGrid :: LightGrid
-initialGrid = listArray ((0, 0), (999, 999)) $ repeat Off
+initialGrid = listArray ((0, 0), (999, 999)) $ repeat 0
 
 update :: Ix i => (e -> e) -> Array i e -> [i] -> Array i e
 update f a indices = accum (flip ($)) a (zip indices (repeat f))
@@ -30,5 +28,24 @@ executeInstruction :: LightGrid -> Instruction -> LightGrid
 executeInstruction grid (operation, bottomLeft, topRight) =
     update operation grid (range (bottomLeft,topRight))
 
+executeInstructions :: [Instruction] -> LightGrid
+executeInstructions = foldl' executeInstruction initialGrid
+
 howManyLights :: [Instruction] -> Int
-howManyLights = length . filter (== On) . elems . foldl' executeInstruction initialGrid
+howManyLights = length . filter (== 1) . elems . executeInstructions
+
+-- Part 2
+
+inc1 :: Light -> Light
+inc1 = (+ 1)
+
+dec1 :: Light -> Light
+dec1 brightness
+    | brightness > 0 = brightness - 1
+    | otherwise      = 0
+
+inc2 :: Light -> Light
+inc2 = (+ 2)
+
+totalBrightness :: [Instruction] -> Int
+totalBrightness = sum . elems . executeInstructions
