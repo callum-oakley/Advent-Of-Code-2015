dbcb2b8aaaa680ade7bd37bf6e12e634a75f3179
Callum Oakley <c.oakley108@gmail.com>
2015-12-10 09:57:03 +0000

    rediscovered the group function

--- Day-10-Elves-Look-Elves-Say.hs
+++ Day-10-Elves-Look-Elves-Say.hs
@@ -1,18 +1,10 @@
 -- Part 1
 import Data.List
 
-look :: String -> [(Int, Char)]
-look (x:xs) = reverse . foldl' countConsecutiveChars [(1, x)] $ xs
-  where
-    countConsecutiveChars acc@((n, y):ys) z
-        | y == z    = (n + 1, y):ys
-        | otherwise = (1, z):acc
-
-say :: [(Int, Char)] -> String
-say = concatMap (\(n, x) -> (show n) ++ [x])
-
 lookAndSay :: String -> [String]
-lookAndSay = iterate (say . look)
+lookAndSay = iterate (concatMap say . group)
+  where
+    say xs = (show . length $ xs) ++ [head xs]
 
 lengthOfResult :: String -> Int -> Int
 lengthOfResult seed n = length . head . reverse . take n . lookAndSay $ seed
