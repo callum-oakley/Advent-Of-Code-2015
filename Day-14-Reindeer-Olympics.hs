36c1064a007d45c5993ec790a0fd17b41fcba0c7
Callum Oakley <c.oakley108@gmail.com>
2015-12-14 11:32:55 +0000

    Day 14 part 2

--- Day-14-Reindeer-Olympics.hs
+++ Day-14-Reindeer-Olympics.hs
@@ -1,14 +1,35 @@
 -- Part 1
+import Data.Array
+import Data.List
+
 type Speed    = Integer
 type Time     = Integer
 type Distance = Integer
-type Reindeer = (Speed, Time, Time)
+type Reindeer = Integer
 
-distance :: Time -> Reindeer -> Distance
+distance :: Time -> (Speed, Time, Time) -> Distance
 distance time (speed, stamina, rest) = timeFlying * speed
   where
     timeFlying = (div time (stamina + rest)) * stamina +
         min stamina (mod time (stamina + rest))
 
-winningDistance :: Time -> [Reindeer] -> Distance
-winningDistance time = maximum . map (distance time)
+winningReindeer :: Time -> Array Reindeer (Speed, Time, Time) -> Reindeer
+winningReindeer time stats = maximumBy compareDistance $ [0..n]
+  where
+    n = snd . bounds $ stats
+    compareDistance x y =
+        compare (distance time (stats ! x)) (distance time (stats ! y))
+
+winningDistance :: Time -> Array Reindeer (Speed, Time, Time) -> Distance
+winningDistance time stats =
+    distance time (stats ! (winningReindeer time stats))
+
+-- Part 2
+update :: Ix i => (e -> e) -> Array i e -> [i] -> Array i e
+update f a indices = accum (flip ($)) a (zip indices (repeat f))
+
+maxPoints :: Time -> Array Reindeer (Speed, Time, Time) -> Integer
+maxPoints time stats = maximum . elems . foldl' f zeroArray $ [1..time]
+  where
+    zeroArray = listArray (bounds stats) (repeat 0)
+    f points t = update succ points [winningReindeer t stats]
