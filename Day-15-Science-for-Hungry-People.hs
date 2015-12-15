1e91487012ac21a1850746aca8f61756b603f7e1
Callum Oakley <c.oakley108@gmail.com>
2015-12-15 11:27:07 +0000

    Day 15 part 2

--- Day-15-Science-for-Hungry-People.hs
+++ Day-15-Science-for-Hungry-People.hs
@@ -3,15 +3,28 @@ import Data.List
 import Math.Combinat.Partitions.Integer
 
 type Ingredient = [Int]
+type Recipe     = [Int]
 
-combinations :: Int -> [[Int]]
-combinations n = concatMap (permutations . pad) $ _partitions' (100, n) 100
+recipes :: Int -> [Recipe]
+recipes n = concatMap (permutations . pad) $ _partitions' (100, n) 100
   where
     pad p = (p ++) $ replicate (n - length p) 0
 
-maxScore :: [Ingredient] -> Int
-maxScore ingredients = maximum . map score . combinations . length $ ingredients
+score :: (Int -> Int -> Int) -> [Ingredient] -> Recipe -> Int
+score f ingredients recipe = calorieTransformation . product . map (max 0) .
+    init $ cookieProperties
   where
-      multiply ingredient multiplier = map (* multiplier) ingredient
-      score = product . map (max 0) . foldr1 (zipWith (+)) .
-        zipWith multiply ingredients
+    cookieProperties = foldr1 (zipWith (+)) $
+        zipWith (\i m -> map (* m) i) ingredients recipe
+    calorieTransformation = f . last $ cookieProperties
+
+maxScore :: (Int -> Int -> Int) -> [Ingredient] -> Int
+maxScore f ingredients = maximum . map (score f ingredients) . recipes .
+    length $ ingredients
+
+maxScoreIgnoringCalories :: [Ingredient] -> Int
+maxScoreIgnoringCalories = maxScore (const id)
+
+-- Part 2
+maxScoreWithFixedCalories :: Int -> [Ingredient] -> Int
+maxScoreWithFixedCalories n = maxScore (\x -> if x == n then id else const 0)
