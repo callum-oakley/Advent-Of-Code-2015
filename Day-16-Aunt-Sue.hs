52777a40865b8c0ee89497e0f0d8c3450c227293
Callum Oakley <c.oakley108@gmail.com>
2015-12-16 11:12:33 +0000

    Day 16 part 2

--- Day-16-Aunt-Sue.hs
+++ Day-16-Aunt-Sue.hs
@@ -5,5 +5,21 @@ import qualified Data.Map.Lazy as Map
 
 type Sue = Map.Map String Int
 
+matchingSueGeneral :: (Sue -> Sue -> Bool) -> Sue -> [Sue] -> Int
+matchingSueGeneral p tape = succ . fromJust . findIndex (p tape)
+
 matchingSue :: Sue -> [Sue] -> Int
-matchingSue tape = succ . fromJust . findIndex (flip Map.isSubmapOf tape)
+matchingSue = matchingSueGeneral (flip Map.isSubmapOf)
+
+-- Part 2
+isMatching :: Sue -> Sue -> Bool
+isMatching tape sue = and . map (\(op, w) -> Map.isSubmapOfBy op w tape) $
+    [((<), xs), ((>), ys), ((==), zs)]
+  where
+    (xs, remaining) =
+        Map.partitionWithKey (\k _ -> elem k ["pomeranians", "goldfish"]) sue
+    (ys, zs) =
+        Map.partitionWithKey (\k _ -> elem k ["cats", "trees"]) remaining
+
+matchingSue' :: Sue -> [Sue] -> Int
+matchingSue' = matchingSueGeneral isMatching
