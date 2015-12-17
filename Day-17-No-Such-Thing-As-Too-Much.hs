7066716ba4687dc54233d1c9f62351b743b6af2d
Callum Oakley <c.oakley108@gmail.com>
2015-12-17 10:31:56 +0000

    Day 17 part 2

--- Day-17-No-Such-Thing-As-Too-Much.hs
+++ Day-17-No-Such-Thing-As-Too-Much.hs
@@ -5,11 +5,19 @@ import Data.List
 type Container = Int
 type Eggnog = Int
 
-eggnogCombos :: [Container] -> Eggnog -> Int
+eggnogCombos :: [Container] -> [Container] -> Eggnog -> [[Container]]
 eggnogCombos = m
   where
-    eggnogCombos _ 0 = 1
-    eggnogCombos containers eggnog = sum . map (\(c, i) ->
-        if (c > eggnog) then 0
-            else (m (drop i containers)  (eggnog - c))) $ zip containers [1..]
+    eggnogCombos _    used 0      = [used]
+    eggnogCombos free used eggnog = concatMap (\(c, i) ->
+        if (c > eggnog) then []
+            else (m (drop i free) (c:used) (eggnog - c))) $ zip free [1..]
     m = memoize eggnogCombos
+
+noOfCombos :: [Container] -> Eggnog -> Int
+noOfCombos containers = length . eggnogCombos containers []
+
+-- Part 2
+noOfMinimalCombos :: [Container] -> Eggnog -> Int
+noOfMinimalCombos containers =
+    length . head . group . sort . map length . eggnogCombos containers []
